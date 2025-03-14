f92b6308e6e1e181f1cfdb265c21fc780fc6fbac
moreen karimi <moreenk541@gmail.com>
2025-03-15 01:39:51 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const newArray = []; 
+
+    for (let i = 0; i < names.length; i++) { 
+        const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        newArray.push(message); 
+    }
+
+    return newArray; 
+}
+
+const messages = writeCards(["Charlie", "Samip", "Ali"], "birthday");
+console.log(messages);
+
+
+function countDown(){
+    let num =10
+    while (num >=0){
+        console.log(num)
+        num--
+        
+    }
+}
+countDown()
